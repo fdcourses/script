0a992742318399a9f5c32f99850ec61050447170
Kirill <freshjs.ftd@gmail.com>
2021-07-09 16:09:19 +0300

    Add variables examples

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,11 @@
-alert('message');
+let variable; // обьявляем переменную
+variable = true; // приваиваем перемменной значение
+
+let _ ; // lodash
+let $ ; // jQuery
+
+// camel case
+let veryLongVariableName = 'good name';  // инициализация перемееной
+
+console.log(veryLongVariableName);
+
