9456fcacb303c8cec97bd9a7d112545e00f0ded1
Kirill <freshjs.ftd@gmail.com>
2021-07-12 15:41:47 +0300

    Add increment and decrement operators

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,11 @@
 
-let num1 = -5;
-let num2 = 2;
+let value = 0;
+ 
+// value++;
+// ++value;
 
-console.log('сложение',num1 + num2);
+console.log(++value); // 1
+console.log(value++); // 1
+console.log(value); // 2
 
-console.log('вычитание',num1 - num2);
-
-console.log('умножение',num1 * num2);
-
-console.log('деление',num1 / num2);
-
-console.log('остаток от деления',num1 % num2);
-
-console.log('возведение в степень',num1 ** num2);
+console.log(--value); // 1
