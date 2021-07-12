ae63047595b0fae57a3711f08376645d8adcd62a
Kirill <freshjs.ftd@gmail.com>
2021-07-12 15:12:47 +0300

    Add arithmetic operators

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,15 @@
 
-let value = null;
+let num1 = -5;
+let num2 = 2;
 
-const test = {
-  value1: undefined,
-  value2: null
-}
+console.log('сложение',num1 + num2);
 
-console.log(test.value1);
-console.log(value);
-console.log(test.value3);
+console.log('вычитание',num1 - num2);
+
+console.log('умножение',num1 * num2);
+
+console.log('деление',num1 / num2);
+
+console.log('остаток от деления',num1 % num2);
+
+console.log('возведение в степень',num1 ** num2);
