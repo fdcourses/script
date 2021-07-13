e66038684ba6d7435a8da74d0e822f29205f7002
Kirill <freshjs.ftd@gmail.com>
2021-07-13 15:47:13 +0300

    Add if...else constraction

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,13 @@
-const myName = prompt('Enter your name:'); 
+const number1 = +prompt('Enter first number');
+const number2 = +prompt('Enter second number');
 
-alert('Hello ' + myName);
+
+
+if(isNaN(number1 - number2)) {
+  alert('Incorrect data');
+}
+else {
+  const result = number1 + number2;
+  
+  alert(result);
+}
