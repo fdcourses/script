e6a789422dd5dc407519b65e6eb2ab370df404f2
Kirill <freshjs.ftd@gmail.com>
2021-07-13 16:00:23 +0300

    Add first function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,18 @@
-const number1 = +prompt('Enter first number');
-const number2 = +prompt('Enter second number');
+// const number1 = +prompt('Enter first number');
+// const number2 = +prompt('Enter second number');
 
 
 
-if(isNaN(number1 - number2)) {
-  alert('Incorrect data');
-}
-else {
-  const result = number1 + number2;
+// if(isNaN(number1 - number2)) {
+//   alert('Incorrect data');
+// }
+// else {
+//   const result = number1 + number2;
   
-  alert(result);
+//   alert(result);
+// }
+
+// function expression
+const myFirstJSFunc = function() {
+  alert('Hello WORLD');
 }
