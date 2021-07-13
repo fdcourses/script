c6c1a3c22037ddec2c7e07523aa941d2693db1b9
Kirill <freshjs.ftd@gmail.com>
2021-07-13 17:01:23 +0300

    Add tasks

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,20 +3,24 @@ const getSumOfTwoNumbers = function (value1, value2) {
   // debugger;
 
   if (isNaN(value1 - value2)) {
-    alert("Incorrect data");
-
-    return null;
+     return null;
   } else {
     const result = value1 + value2;
 
-    alert(result);
-
     return result;
-
-    alert(result);
   }
 };
 
 const result = getSumOfTwoNumbers("test", 15);
 
 console.log(result);
+
+// Найти минимальное число из 2
+
+// Найти максимальное число из 2 
+
+// Проверить, является ли число четным
+
+// + С помощью функции решите квадратное уравнение
+
+// a*x^2 + b*x + c = 0
