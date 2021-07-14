7ff22202c73e6d143c3331e2cf9ad6e4c9afc155
Kirill <freshjs.ftd@gmail.com>
2021-07-14 16:32:39 +0300

    Add switch

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,14 +2,24 @@ const userInput = +prompt("Меню:\n1-сок\n2-чай\n3-компот\n4-то
 
 // alert("Петя сказал: \"Привет\"");
 
-if (userInput === 1) {
-  alert("Вы выбрали сок");
-} else if (userInput === 2) {
-  alert("Вы выбрали чай");
-} else if (userInput === 3) {
-  alert("Вы выбрали компот");
-} else if (userInput === 4) {
-  alert("Вы выбрали торт");
-} else {
-  alert("No");
+switch(userInput) {
+  case 1: {
+    alert("Вы выбрали сок");
+    break;
+  }
+  case 2: {
+    alert("Вы выбрали чай");
+    break;
+  }
+  case 3: {
+    alert("Вы выбрали компот");
+    break;
+  }
+  case 4: {
+    alert("Вы выбрали торт");
+    break;
+  }
+  default: {
+    alert('Нет такого пункта');
+  }
 }
