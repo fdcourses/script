8192de0c8eccab9b17489f5d69e0f67322156801
Kirill <freshjs.ftd@gmail.com>
2021-07-14 15:05:41 +0300

    Add task solutions

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,87 @@
 // function expression
-const getSumOfTwoNumbers = function (value1, value2) {
-  // debugger;
+// const getSumOfTwoNumbers = function (value1, value2) {
+//   // debugger;
 
-  if (isNaN(value1 - value2)) {
-     return null;
-  } else {
-    const result = value1 + value2;
+//   if (isNaN(value1 - value2)) {
+//      return null;
+//   } else {
+//     const result = value1 + value2;
 
-    return result;
-  }
+//     return result;
+//   }
+// };
+
+// const result = getSumOfTwoNumbers("test", 15);
+
+const getSum = function (a, b) {
+  const result = a + b;
+
+  return result;
 };
 
-const result = getSumOfTwoNumbers("test", 15);
-
-console.log(result);
+// console.log(result);
 
 // Найти минимальное число из 2
+const getMinNumber = function (num1, num2) {
+  if (isNaN(num1 - num2)) {
+    return null;
+  }
 
-// Найти максимальное число из 2 
+  if (num1 > num2) {
+    return num2;
+  }
+  return num1;
+};
+
+// Найти максимальное число из 2
+const getMaxNumber = function (num1, num2) {
+  if (isNaN(num1) || isNaN(num2) ) {
+    return null;
+  }
+
+  if (num1 > num2) {
+    return num1;
+  }
+  return num2;
+};
 
 // Проверить, является ли число четным
+const isEven = function (number) {
+  if (isNaN(number)) {
+    return null;
+  }
+
+  // if(number % 2 === 0) {
+  //   return true;
+  // }
+  // return false;
+
+  return number % 2 === 0;
+}
 
 // + С помощью функции решите квадратное уравнение
 
-// a*x^2 + b*x + c = 0
+const solveSquareExpression = function (a,b,c) {
+  if (isNaN(a-b-c)) {
+    return null;
+  }
+
+  const d = b * b - 4 * a * c;
+
+  if(d < 0) {
+    return NaN;
+  }
+
+  if (d === 0) {
+    const x = -b / (2 * a);
+    return [x, x]; // число
+  }
+
+  const dSquareRoot = d ** (1/2);
+
+  const x1 = (-b + dSquareRoot) / (2 * a);
+  const x2 = (-b - dSquareRoot) / (2 * a);
+
+  return [x1, x2]; 
+}
+
