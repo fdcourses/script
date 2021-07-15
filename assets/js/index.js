ac6a0092b0a85ef1b6982c0b24f23d8d575965a1
Kirill <freshjs.ftd@gmail.com>
2021-07-15 14:19:34 +0300

    Add switch months example

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,6 @@
 // const number2;
 // alert("Петя сказал: \"Привет\"");
 
-
 // switch(userInput) {
 //   case 1: {
 //     alert("Вы выбрали сок");
@@ -37,28 +36,74 @@
 // const number2 = +prompt('number 2');
 // const operator = prompt('Enter operator');
 
+const month = +prompt('Enter month');
 
+// if (result === 1) {
+//   // 1
+// } else if (result === 2) {
+//   // 2
+// } else {
+//   // все остальное
+// }
+
+
+// if(month > 2 && month < 6) {
+//   alert('Spring');
+// }
+
+
+switch (month) {
+  case 12:
+  case 1:
+  case 2: {
+    // 2
+    alert("Зима");
+    break;
+  }
+  case 3:
+  case 4:
+  case 5: {
+    alert('Spring');
+    break;
+  }
+  case 6:
+  case 7:
+  case 8: {
+    alert('Summer');
+    break;
+  }
+  case 9:
+  case 10:
+  case 11: {
+    alert('Autumn');
+    break;
+  }
+
+  default: {
+    // все остальное
+    alert("Doesnt exist");
+  }
+}
 
 const calculate = function (number1, number2, operator) {
-
-  if(isNaN(number1 - number2)) {
+  if (isNaN(number1 - number2)) {
     return null;
   }
 
-  switch(operator) {
-    case '+': {
+  switch (operator) {
+    case "+": {
       const result = number1 + number2;
       return result;
     }
-    case '-': {
+    case "-": {
       const result = number1 - number2;
       return result;
     }
-    case '*': {
+    case "*": {
       const result = number1 * number2;
       return result;
     }
-    case '/': {
+    case "/": {
       const result = number1 / number2;
       return result;
     }
@@ -66,4 +111,4 @@ const calculate = function (number1, number2, operator) {
       return null;
     }
   }
-}
+};
