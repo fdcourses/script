946daa3436e613e5052a116721ac7f10f656859b
Kirill <freshjs.ftd@gmail.com>
2021-07-19 17:08:26 +0300

    Add property accessor

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,28 +17,11 @@ const user1 = {
   firstName: 'Test',
   lastName: 'Testovich',
   age: 18,
-  isMale: true
+  isMale: true,
+  0 : 'secret',
+  'full name': 'Test Testovich'
 }
 
-const user2 = {
-  firstName: 'Null',
-  lastName: 'Nullovna',
-  age: 18,
-  isMale: false
-}
+const value = 'full name';
 
-const user3 = {
-  firstName: 'Undefined',
-  lastName: 'Undefinov',
-  age: 18,
-  isMale: true
-}
-
-// Создайте функцию которая будет здороватся с пользователем
-
-function sayHello(user) {
-  return `Hello ${user.firstName} ${user.lastName} !`;
-}
-
-// const gretings1 = ;
-sayHello(user1);
+console.log(value);
