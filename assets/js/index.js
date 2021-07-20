dfcd1361fe88ee6be343022122bff1ee710e2f88
Kirill <freshjs.ftd@gmail.com>
2021-07-20 16:30:25 +0300

    Add arrays

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,44 +1,12 @@
-/*
-1. Создать функцию - конструктор обьектов пользователей 
-   со следующими свойствами:
-    firstName
-    lastName
-    fullName
-    age
-    isMale
 
-  + 1) у пользователья есть свойство отвечающее за его деньги 
-        в банке
-     2) свойство от вечающее за его зарплату
-     3) ему можно было начисял ть зарплату 
-*/
 
-const boss = {
-  firstName: 'Petr',
-  lastName: 'Pertovich',
-  age: 37,
-  isMale: true,
-  0 : 'secret',
-  'full name': 'Pert Petrovich',
+const tel1 = '1356456453';
+const tel2 = '3543545746';
+
+const myPhoneNumbers = {
+  1 : '1356456453',
+  0 : '3543545746',
 }
 
-const test = 0;
-const newProperty = 'abracadabra 123456789';
-
-
-// console.log(boss[test]);
-
-
-function createMultiplicationTable() {
-  const resultingTable = {};
-  
-  for(let i = 1; i < 10; i++) {
-    for(let j = 1; j < 10; j++) {
-      resultingTable[`${i} * ${j} = `] = j * i;
-    }
-  }
-
-  return resultingTable;
-}
-
-createMultiplicationTable();
+const phonesArray = ['1356456453', '3543545746'];
+const phonesArray2 = new Array('1356456453', '3543545746');
