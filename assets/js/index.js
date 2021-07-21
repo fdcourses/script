53e1ef0dff49193dd9f10c0f606764505fc8340d
Kirill <freshjs.ftd@gmail.com>
2021-07-21 16:27:03 +0300

    Add prototype inheritance

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,6 +17,56 @@ const secondDeleted = arr.pop();
 
 // ================================================
 
+const test1 = {};
+const test2 = {};
+
+function Student (firstName, lastName, age, isMale) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+  this.isMale = isMale;
+
+  this.study = study;
+}
+
+function study () {
+  return `Student ${this.firstName} ${this.lastName} is studying`;
+}
+
+const student1 = new Student('Test', 'Testovich', 22, true);
+const student2 = new Student('Test1', 'Testovich1', 221, true);
+
+function Animal () {
+
+  this.exist = function() {
+    return true;
+  } 
+}
+
+const monkeyPrototype =  new Animal();
 
 
-console.log(arr);
+
+function Monkey (name, appendages) {
+  this.name = name;
+  this.appendages = appendages;
+}
+
+Monkey.prototype = monkeyPrototype;
+
+
+const pete = new Monkey ('Pete', 5);
+
+const tom = new Monkey ('Tom', 5);
+
+const jerry = new Monkey ('jerry', 5);
+
+
+const testasdsadas = new Monkey ('testasdsadas', 5);
+
+
+const sadsa = new Monkey ('asdsad', 5);
+
+
+// pete.__proto__ = monkeyPrototype;
+// tom.__proto__ = monkeyPrototype;
