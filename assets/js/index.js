ea7a17245160e6ea48f511c86ba4a8b0adb128f7
Kirill <freshjs.ftd@gmail.com>
2021-07-22 17:16:51 +0300

    Add getMin and getMax

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,39 @@
 
-const arr = [];
+const arr = [-100,5,30,45,60,1];
 
 // ================================================
 
+function getMin (arrayOfNumbers) {
+  let minNumber = arrayOfNumbers[0];
+
+  for(let i = 0; i < arrayOfNumbers.length ;i++){
+    if(arrayOfNumbers[i] < minNumber) {
+      minNumber = arrayOfNumbers[i];
+    }
+  }
+
+  return minNumber;
+}
+
+
+function getMax (arrayOfNumbers) {
+  let maxNumber = arrayOfNumbers[0];
+
+  for(let i = 0; i < arrayOfNumbers.length ;i++){
+    if(arrayOfNumbers[i] > maxNumber) {
+      maxNumber = arrayOfNumbers[i];
+    }
+  }
+
+  return maxNumber;
+}
+
+function getRandomNumber( ) {
+  return Math.round(Math.random() *100);
+}
+
+const arr2 = [];
+
+for(let i = 0; i < 10; i++) {
+  arr2[i] = getRandomNumber();
+}
