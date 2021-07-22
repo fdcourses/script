c2dc0439e2d5b9342eae687d709e74237ac50454
Kirill <freshjs.ftd@gmail.com>
2021-07-22 14:01:58 +0300

    Refactor project structure

---
+++ assets/js/MyArray.js
@@ -0,0 +1,20 @@
+function MyArrayProtoProto () {
+  
+}
+
+function MyArrayProto() {
+  this.push = function (element) {
+    // console.log(arguments);
+    this[this.length++] = element;
+    return this.length;
+  }
+}
+
+function MyArray () {
+  this.length = 0;
+}
+
+MyArray.prototype = new MyArrayProto();
+
+const myArr = new MyArray();
+const myArr2 = new MyArray();
--- assets/js/index.js
+++ assets/js/index.js
@@ -3,23 +3,3 @@ const arr = [];
 
 // ================================================
 
-function MyArrayProtoProto () {
-  
-}
-
-function MyArrayProto() {
-  this.push = function (element) {
-    // console.log(arguments);
-    this[this.length++] = element;
-    return this.length;
-  }
-}
-
-function MyArray () {
-  this.length = 0;
-}
-
-MyArray.prototype = new MyArrayProto();
-
-const myArr = new MyArray();
-const myArr2 = new MyArray();
