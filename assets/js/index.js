378996bb93915152ebf5e5cd73bb24ba7e579f55
Kirill <freshjs.ftd@gmail.com>
2021-07-23 17:14:04 +0300

    Add forEach

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,37 +3,50 @@ const arr = [-100,5,30,45,60,1];
 
 // ================================================
 
-function getMin (arrayOfNumbers) {
-  let minNumber = arrayOfNumbers[0];
-
-  for(let i = 0; i < arrayOfNumbers.length ;i++){
-    if(arrayOfNumbers[i] < minNumber) {
-      minNumber = arrayOfNumbers[i];
-    }
+for(let i = 0; i < arr.length; i++) {
+  console.log(arr[i] * 2);
   }
 
-  return minNumber;
+function double (number) {
+  const result = number * 2;
+  console.log(result);
+  return result;
 }
 
+arr.forEach(double);
 
-function getMax (arrayOfNumbers) {
-  let maxNumber = arrayOfNumbers[0];
+arr.forEach(function (number) {
+  console.log(number * 2);
+});
 
-  for(let i = 0; i < arrayOfNumbers.length ;i++){
-    if(arrayOfNumbers[i] > maxNumber) {
-      maxNumber = arrayOfNumbers[i];
-    }
+arr.forEach(function (elem) {
+  console.log(elem);
+});
+
+const users = [
+  { firstName: "User1", lastName: "Test", age: 20 },
+  { firstName: "User2", lastName: "Test", age: 13 },
+  { firstName: "User3", lastName: "Test", age: 50 },
+  { firstName: "User4", lastName: "Test", age: 77 },
+  { firstName: "User5", lastName: "Test", age: 45 },
+  { firstName: "User6", lastName: "Test", age: 19 },
+  { firstName: "User7", lastName: "Test", age: 415 },
+];
+
+function greet(user) {
+  const greeting = `Привет ${user.firstName} ${user.lastName}`;
+  console.log(greeting);
+  return greeting;
   }
 
-  return maxNumber;
+users.forEach(greet);
+
+// foreach  без foreach
+// for(let i = 0; i < users.length;i++) {
+//   greet(users[i]);
+// }
+
 }
 
-function getRandomNumber( ) {
-  return Math.round(Math.random() *100);
-}
 
-const arr2 = [];
 
-for(let i = 0; i < 10; i++) {
-  arr2[i] = getRandomNumber();
-}
