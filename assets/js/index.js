10e5f03e4adc64ba992d08c02bbb99ed188da9af
Kirill <freshjs.ftd@gmail.com>
2021-07-23 17:14:58 +0300

    Add some & every

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,52 +1,18 @@
 
-const arr = [-100,5,30,45,60,1];
-
-// ================================================
-
-for(let i = 0; i < arr.length; i++) {
-  console.log(arr[i] * 2);
-  }
-
-function double (number) {
-  const result = number * 2;
-  console.log(result);
-  return result;
+function isAdult(user) {
+  return user.age >= 18;
 }
 
-arr.forEach(double);
+const someVerdict = users.some(isAdult);
 
-arr.forEach(function (number) {
-  console.log(number * 2);
-});
+const everyVerdict = users.every(isAdult);
 
-arr.forEach(function (elem) {
-  console.log(elem);
-});
-
-const users = [
-  { firstName: "User1", lastName: "Test", age: 20 },
+const myUsers = new MyArray(
   { firstName: "User2", lastName: "Test", age: 13 },
+  { firstName: "User1", lastName: "Test", age: 20 },
   { firstName: "User3", lastName: "Test", age: 50 },
   { firstName: "User4", lastName: "Test", age: 77 },
   { firstName: "User5", lastName: "Test", age: 45 },
   { firstName: "User6", lastName: "Test", age: 19 },
-  { firstName: "User7", lastName: "Test", age: 415 },
-];
-
-function greet(user) {
-  const greeting = `Привет ${user.firstName} ${user.lastName}`;
-  console.log(greeting);
-  return greeting;
-  }
-
-users.forEach(greet);
-
-// foreach  без foreach
-// for(let i = 0; i < users.length;i++) {
-//   greet(users[i]);
-// }
-
-}
-
-
-
+  { firstName: "User7", lastName: "Test", age: 415 }
+);
