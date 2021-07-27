06b75ad1437c16504cff173f02a2a953ca8db8f6
Kirill <freshjs.ftd@gmail.com>
2021-07-27 17:06:42 +0300

    Add high order functions example

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,3 +18,20 @@ function square (number) {
 
   return result;
 }
+
+function highOrderFunction ( callbackFunction, num1, num2 ) {
+  return callbackFunction(num1, num2);
+}
+
+const hofSum = highOrderFunction(function (elem, elem2) {  
+  return elem + elem2;
+}, 5, 5);
+
+function hof () {
+
+  return function() {
+    return true;
+  }
+}
+
+const test = hof();
