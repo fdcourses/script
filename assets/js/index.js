e264696439376aeb35e65461dc924d25a898f898
Kirill <freshjs.ftd@gmail.com>
2021-07-29 16:56:01 +0300

    Add rest && spread operators

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,5 @@
 "use strict";
 
-// functional expression
-const anotherSum = function (a, b) {
-  return this;
-};
-
-// function decrlaration
-function sum(a, b) {
-  return this;
-}
-
-const arrowSum = (a, b) => {
-  return this;
-};
-
 const newspaper = {
   title: "NY Post - Murzilka",
 
@@ -21,12 +7,45 @@ const newspaper = {
 
   showArticles() {
     const that = this;
-    this.articles.forEach((article, index, array) => {
-      console.log(
-        `Newspaper ${this.title} | Article №${index + 1} : ${article}`
-      );
-    });
+
+    const arrow3 = () => {
+      console.log(this);
+    };
+
+    this.articles.forEach();
+
+    for (let i = 0; i < this.articles.length; i++) {
+      const arrow = (article, index, array) => {
+        console.log(
+          `Newspaper ${this.title} | Article №${index + 1} : ${article}`
+        );
+      };
+    }
   },
 };
 // сделатйе следующий вывод для метода showArticles
 // NewsPaper <title> Article №<i>: <article>
+
+const arr = [1, 2, 3, 4];
+
+const evenArr = arr.filter((number) => number % 2 === 0);
+
+function sum() {
+  let result = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    result += arguments[i];
+  }
+  return result;
+}
+
+// rest operator - собирает любое количество в 1 массив
+const arrowSum = (...rest) => rest.reduce((accumulator, currentValue) => accumulator + currentValue);
+
+const test1 = arrowSum(1, 2, 3, 4, 5);
+
+const numbers = [1,2,4,5,6,8,10];
+const numbers2 = [1,25,8,66,6,3];
+
+// spread - разворпвчивает из массива 
+const allNumbers = [...numbers, ...numbers2];
+const allNumbersExtended = [1,2,4,5,6,8,10, 1,25,8,66,6,3];
