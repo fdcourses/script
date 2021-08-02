24930d4abcdf808804d75dbea4dbc0c87e8d8221
Kirill <freshjs.ftd@gmail.com>
2021-08-02 16:37:33 +0300

    Add gettserts/setters

--- assets/js/coolerPrototype.js
+++ assets/js/coolerPrototype.js
@@ -1,24 +1,31 @@
 'use strict';
 
-class CoolerHuman {
+class Human {
   constructor(name, lastName, age) {
     // свойства созадваемого экземпляра класса
 
-    if(isNaN(age) || typeof age !== 'number') {
-      throw new TypeError('Age must be number');
-    }
-
     // проверьте что пользователь имеет 
     // не отрицательный возраст при созддании
-    if(age < 0) {
-      throw new RangeError('Age must be positive nubmer');
-    }
-
-    
 
     this.name = name;
     this.lastName = lastName;
     this.age = age;
+  }
+
+  get age() {
+    return this._age;
+  }
+
+  set age(newAge) {
+    if(isNaN(newAge) || typeof newAge !== 'number') {
+      throw new TypeError('Age must be number');
+    }
+
+    if(newAge < 0) {
+      throw new RangeError('Age must be positive nubmer');
+    }
+
+    this._age = newAge;
   }
 
   // методы у экземпляров класса
@@ -34,5 +41,39 @@ class CoolerHuman {
   }
 }
 
-const coolerHuman1 = new CoolerHuman('Coolkid', 'Coolkidovich', 20);
-const coolerHuman2 = new CoolerHuman('Coolkid', 'Coolkidovich', 50);
+const coolerHuman1 = new Human('Coolkid', 'Coolkidovich', 20);
+const coolerHuman2 = new Human('Coolkid', 'Coolkidovich', 50);
+
+const human1 = new Human('Human', 'Feofanov', 40 );
+
+const obj = {
+  // get food() {
+  //   return 'smacna xavka';
+  // }
+
+  set food (newFood) {
+    this._food = newFood;
+  },
+
+  get unTastyFood() {
+    return this._food;
+  }
+}
+
+/*
+  Создать класс машина
+  У машины будут следующие свойства:
+  вес корпуса,
+  максимальная скорость,
+  текущую скорость, 
+  имя
+
+  всем этим свойствам задать геттеры и сеттеры
+  осуществить в сеттерах проверки входящих данных 
+  скрость не может ыбть отрицательной
+  вес тоже не отрицательный 
+  имя не может быть пустым
+
+*/
+
+
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,1 @@
 'use strict';
-function Human (name, lastName) {
-  this.name = name;
-  this.lastName = lastName;
-
-} 
-function HumanProto () {
-  this.walk = function () {
-    console.log('I am walking right now');
-  }
-}
-
-Human.prototype = new HumanProto();
-
-const h1 = new Human('Test', 'Testovich');
