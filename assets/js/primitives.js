ea5b0811b5c6f15dc5807166e37cd5724f8e11eb
Kirill <freshjs.ftd@gmail.com>
2021-08-05 16:40:06 +0300

    Add Jaden Case

--- assets/js/primitives.js
+++ assets/js/primitives.js
@@ -1,46 +1,37 @@
-'use strict';
+"use strict";
 
-const str = '       secret        test    very    secret        ';
+const str = "secret test very secret";
+const str2 = "Nikolay Petrovich Ovechkin";
 const strObj = new String(str);
-
 
 const firstLetter = str.charAt(0);
 const firstLetterUnicodeCode = str.charCodeAt(0);
-const hasLetter = str.includes('t');
+const hasLetter = str.includes("t");
 
 const trimmed = str.trim();
 
-const newStr = str.replace('secret', 'null');
+const newStr = str.replace("secret", "null");
+
+const strArr = str2.split("");
+const str2Twin = strArr.join("");
+
+// Hello There My Dear Friends I Wand To Speak With You
+
+function toJadenCase(str) {
+  // const words = str.split(' ');
+  // const wordsWithUpper = words.map((word) => {
+  //   const firstLetter = word[0].toUpperCase();
+  //   return firstLetter + word.slice(1);
+  // });
+  // const result = wordsWithUpper.join(' ');
+  // return result;
+
+  return str
+    .split(" ")
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(" ");
 
 
-function makeStringCaps (str) {
-  let result = '';
-
-  for(let i = 0; i < str.length; i++) {
-    result += str[i].toUpperCase();
-  }
-
-  return result;
 }
-
-// создайте функцию toRandomCase
-
-function toRandomCase (str) {
-  let result = '';
-
-  for(let i = 0; i < str.length; i++) {
-
-    if(Math.random() > 0.5) {
-      result += str[i].toUpperCase();
-    } else {
-      result += str[i].toLowerCase();
-    }
-
-  }
-
-  return result;
-}
-
-// test string > TesT stRInG
 
 console.log(trimmed);
